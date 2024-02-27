4d8a58445902879ed12899c6cc816dd03b187230
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-02-27 15:10:36 -0800

    goon

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,6 @@
 loadText = document.querySelector('.loading-text')
 bg = document.querySelector('.bg')
+console.log("hello")
 
 load = 0
 int = setInterval(blurring, 25)
@@ -11,6 +12,5 @@ function blurring() {
     }
     loadText.innerText = `${load}%`
     loadText.style.opacity = 1 - load/100
-    bg.style.filter = `blur(${30 - (load/100) * 30})`
+    bg.style.filter = `blur(${30 - (load/100) * 30}px)`
 }
-
