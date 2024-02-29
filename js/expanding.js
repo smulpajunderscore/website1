eb53e162f47f920990ee8b7317e7e8397f722a8d
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-02-29 22:34:00 +0000

    Thu, Feb 29, 2024, 2:34 PM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -2,6 +2,13 @@ panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        remove_active_classes()
         panel.classList.toggle('active')
     })
 })
+
+function remove_active_classes(){
+    panels.forEach(panel => {
+        panel.classlist.remove('active')
+    })
+}
