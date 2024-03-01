f021e743072859e34620cf1668f384894fad64e5
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-03-01 22:41:50 +0000

    Fri, Mar 1, 2024, 2:41 PM -08:00

--- js/sound.js
+++ js/sound.js
@@ -3,4 +3,9 @@ audio = ['', '', '', '', '', '']
 audio.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
+    btn.innerText = sound
+
+    doscument.getElementById('buttons').append childs
+
+    
 })
