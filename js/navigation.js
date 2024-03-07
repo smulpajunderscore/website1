981b1cf69c989af4a1801d889d4e36a1f57c6b1c
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-03-07 21:46:53 +0000

    Thu, Mar 7, 2024, 1:46 PM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -3,5 +3,9 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fix_nav)
 
 function fix_nav() {
-    
+    if (window.scrollY > 256) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
 }
