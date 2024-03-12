bb9101f1363c1bada8c781c2a8a1a4da465b66bf
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-03-12 21:44:12 +0000

    Tue, Mar 12, 2024, 2:44 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,5 +1,11 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
+
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show_nav')
 })
 
+open.addEventListener('click', () => {
+    modal.classList.add('show_modal')
+})
