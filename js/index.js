393c4548bfcc67f449d64a8ff78c4dd11822dfb6
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-03-16 23:42:16 +0000

    Sat, Mar 16, 2024, 4:42 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 
 document.documentElement.style.setProperty("--transx", "");
+
+links = ['search', 'loading', 'expland', 'sound', 'navigate', 'landing']
+
+audio.forEach(sound => {
+    btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+    btn.addEventListener('click', () => {
+        stop_audio()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
+})
